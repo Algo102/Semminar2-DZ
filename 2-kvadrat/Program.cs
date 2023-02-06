50cb43e5dd27c805fc579ed6ea02e3d18443ddfb
Algo <algor102@gmail.comm>
2023-02-06 07:33:21 +0500

    Семминар2-Задача2-Является ли одно из чисел квадратом другого

---
+++ 2-kvadrat/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Программа определяет, является ли число квадратом другого");
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat1 = a*a;
+int kvadrat2 = b*b;
+
+if (kvadrat1 == b || kvadrat2 == a) Console.WriteLine ("Одно из введеных чисел является квадратом другого");
+else Console.WriteLine ("Ни одно число НЕ является квадратом другого");
