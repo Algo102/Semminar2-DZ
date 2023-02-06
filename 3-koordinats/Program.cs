576af535b1dc22b0b1b83771e24e5dff251129f8
Algo <algor102@gmail.comm>
2023-02-06 07:45:14 +0500

    Семминар2-Задача3-Подсказка по координатам в зависимости от четверти

---
+++ 3-koordinats/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Программа подсказывает дипозоны по заданной четверти в оси координат");
+Console.WriteLine("Введите необходимую четверть от 1 до 4 включительно");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 1) Console.WriteLine ("Вы можете использовать координаты X>0 и Y>0");
+else if (a == 2) Console.WriteLine ("Вы можете использовать координаты X<0 и Y>0");
+else if (a == 3) Console.WriteLine ("Вы можете использовать координаты X<0 и Y<0");
+else Console.WriteLine ("Вы можете использовать координаты X>0 и Y<0");
+
+Console.WriteLine("Спасибо за внимание, обращайтесь еще");
