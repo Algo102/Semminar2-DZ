5ba633518cf4ad44df624c47fb47e81d9307da74
Algo <algor102@gmail.comm>
2023-02-06 07:22:12 +0500

    2Семминар-1задание-определение выходного дня по номеру

---
+++ 1-holydays/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Программа по заданному дню, говорит это выходной или нет");
+Console.WriteLine("Введите число, соответсвующее дню недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 6 || number == 7) Console.WriteLine ("Этот день выходной");
+else Console.WriteLine ("Этот день не выходной");
